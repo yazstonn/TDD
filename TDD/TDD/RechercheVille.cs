acabb6f8c37a7d33ba60efca371fc6c666df1801
BELEMBERT Melvin <yazstonn@hotmail.fr>
2025-02-07 14:14:10 +0100

    création du constructeur

--- TDD/TDD/RechercheVille.cs
+++ TDD/TDD/RechercheVille.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace TDD
 {
     public class RechercheVille
     {
         private List<String> _villes;
+
+        public RechercheVille()
+        {
+            _villes = new List<String>() { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul" };
+        }
 
         public List<String> Rechercher(String mot)
         {
