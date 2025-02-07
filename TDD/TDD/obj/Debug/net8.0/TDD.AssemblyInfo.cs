f9f1e97a890995f53642531680250e76c83c0053
BELEMBERT Melvin <yazstonn@hotmail.fr>
2025-02-07 14:06:47 +0100

    création solution test

--- TDD/TDD/obj/Debug/net8.0/TDD.AssemblyInfo.cs
+++ TDD/TDD/obj/Debug/net8.0/TDD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2708509ddad9e12fb4bb3568ebbb8af5457367c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e07125622a55a3df980a388b7d9f5d81c5cc47d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
