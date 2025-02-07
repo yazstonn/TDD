a9db701eccfc5ade8a359c20aab203f999e7a417
BELEMBERT Melvin <yazstonn@hotmail.fr>
2025-02-07 14:08:27 +0100

    création de la classe RechercheVille

---
+++ TDD/TDD/RechercheVille.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public class RechercheVille
+    {
+        private List<String> _villes;
+
+        public List<String> Rechercher(String mot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
